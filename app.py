0316c3597cbe4388ba51231585bb444ba9186c1e
Mashiat Nabila <yeasinarifin@Mashiats-MacBook-Air.local>
2026-06-06 14:20:28 -0500

    Fix port configuration

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ TRANSCRIPT:
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=False, host='0.0.0.0', port=port)
